33afa5b03e1e046baae11b06ba0a001860371b45
Bit1100 <you@example.com>
2023-03-02 00:33:17 -0800

    Try to Show Images

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,7 +4,7 @@ export const projects = [
     title: "Admin Dashboard",
     description:
       "This app is made for admin so that he/she can get the insights of the business and even can lookup/modify other users information.",
-    image: "/images/dashboard.png",
+    image: "./images/dashboard.png",
     tags: ["React", "MaterialUI", "Redux"],
     demo: "https://bit1100.github.io/Dashboard_React_App/",
     code: "https://github.com/Bit1100/Dashboard_React_App",
@@ -14,7 +14,7 @@ export const projects = [
     title: "Pro Landing Page",
     description:
       "This is a fully responsive and functional Professional Landing Page for fruit business.",
-    image: "/images/fruits.png",
+    image: "./images/fruits.png",
     tags: ["React", "Typescript", "React-Query"],
     demo: "https://bit1100.github.io/React-Professional-Landing-Page/",
     code: "https://github.com/Bit1100/React-Professional-Landing-Page",
@@ -24,7 +24,7 @@ export const projects = [
     title: "Cadiddates Hiring App",
     description:
       "This app is sort of Hiring Platform where candidates details could be found and the company can view it to hire them. This is the platform for both the candidates and the companies which binds together the job-seeking candidates and hiring companies.",
-    image: "/images/hiring.png",
+    image: "./images/hiring.png",
     tags: ["React", "Typescript", "Redux", "TailwindCSS"],
     demo: "https://bit1100.github.io/internshala-react-app/",
     code: "https://github.com/Bit1100/internshala-react-app",
@@ -34,7 +34,7 @@ export const projects = [
     title: "Blog Website",
     description:
       "This is a NextJs Blog Website where users can read, search and filter the blogs as per their interests.",
-    image: "/images/blog.png",
+    image: "./images/blog.png",
     tags: ["Nextjs", "Styled-components"],
     demo: "https://next-js-blog-website-1.vercel.app/",
     code: "https://github.com/Bit1100/NextJs_Blog_Website",
@@ -44,7 +44,7 @@ export const projects = [
     title: "Free Seminar WebPage",
     description:
       "This is a fully responsive and functional Landing Page which deals with the free seminar for the education training purposes.",
-    image: "/images/event.png",
+    image: "./images/event.png",
     tags: ["Reactjs", "Typescript", "Vanilla CSS"],
     demo: "https://bit1100.github.io/Fully_Responsive_Webpage/",
     code: "https://github.com/Bit1100/Fully_Responsive_Webpage",
@@ -54,7 +54,7 @@ export const projects = [
     title: "ChatApps",
     description:
       "This is a chatapp that make use of socketio library for real-time communication between individuals and groups.",
-    image: "/images/socketio.png",
+    image: "./images/socketio.png",
     tags: ["SocketIO", "NodeJs", "ExpressJs", "hbs"],
     demo: "https://github.com/Bit1100/ChatApps__SocketIO",
     code: "https://github.com/Bit1100/ChatApps__SocketIO",
